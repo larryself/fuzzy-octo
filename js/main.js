72ecafec6511fb3f05c6600f847d77df0d1c5ebb
larryself <yanmusikhin@gmail.com>
2021-01-31 11:06:47 +0400

    add autorized button

--- js/main.js
+++ js/main.js
@@ -1,16 +1,30 @@
-/// modal window
+// modal window
 
 let modal = document.getElementById("myModal");
-let btn = document.getElementById("open");
+let btn = document.getElementById("enter");
 let clos = document.getElementById("close");
-let openCab
+let openCab = document.getElementById("autorized");
+let btnCl = document.getElementById("notAutorized");
+let exitInLk = document.getElementById("exit");
+
 const modalOn = () => {
     modal.style.display = "block";
+
 };
 const modalOff = () => {
     modal.style.display = "none";
+    openCab.style.display = "block";
+    btnCl.style.display = "none";
 }
+const exitIn = () => {
+    openCab.style.display = "none";
+    btnCl.style.display = "block";
+}
+exitInLk.onclick = exitIn;
 btn.onclick = modalOn;
 clos.onclick = modalOff;
 
-/// localstorage
+// localstorage
+
+
+// tabs
