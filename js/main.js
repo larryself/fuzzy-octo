308c8a47e75d20a632b8126dc0816b0b5df73fe3
larryself <yanmusikhin@gmail.com>
2021-02-01 15:01:19 +0400

    add border-bottom

--- js/main.js
+++ js/main.js
@@ -25,6 +25,20 @@ btn.onclick = modalOn;
 clos.onclick = modalOff;
 
 // localstorage
-
+const login = document.getElementsByClassName("form-input_login");
+const clickCheck = document.getElementsByClassName("custom-checkbox");
+const truefalse = () => {
+    if (document.getElementsByClassName("custom-checkbox")[0].checked) {
+        alert(login)
+    }
+}
+clickCheck.onclick = truefalse;
 
 // tabs
+
+console.log(document.querySelectorAll('.main-nav .nav-item'))
+document.querySelectorAll('.main-nav .nav-item').forEach((item, index) => {
+    const element = document.getElementById(`movi`);
+    item.addEventListener('click', () => item.classList.add('nav-item_activ'));
+    item.addEventListener('!click', () => item.classList.toggle('nav-item_activ'));
+});
