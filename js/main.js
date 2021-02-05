a559a42338f1b9e7efd3630e3cfa79f37c9286db
larryself <yanmusikhin@gmail.com>
2021-02-05 16:45:54 +0400

    fixed bad name

--- js/main.js
+++ js/main.js
@@ -1,41 +1,48 @@
 // modal window
+const loginFormModal = document.querySelector(".js-form-modal");
+const openFormLogin = document.querySelector(".js-open-form");
+const logginBtn = document.querySelector(".js-autorized-user");
+const authorizedUserControl = document.querySelector(".js-autorized-user-control");
+const btnLogin = document.querySelector(".js-notautorized-user-control");
+const btnExit = document.querySelector(".js-logout");
+const modalWindow = document.querySelector('.js-bg-modal');
 
-const modal = document.getElementById("myModal");
-const btn = document.getElementById("enter");
-const btnEnter = document.getElementById("close");
-const openCab = document.getElementById("autorized");
-const btnCl = document.getElementById("notAutorized");
-const btnExit = document.getElementById("exit");
+function loginFormOn(e) {
+    e.preventDefault();
+    loginFormModal.classList.add('active');
+}
 
-function modalOn() {
-    modal.style.display = "block";
+function loginFormOff(e) {
+    e.preventDefault();
+    loginFormModal.classList.remove('active');
+    authorizedUserControl.classList.add('active');
+    btnLogin.classList.add('not-active');
 }
-function modalOff() {
-    modal.style.display = "none";
-    openCab.style.display = "block";
-    btnCl.style.display = "none";
-}
-function exitInLK() {
-    openCab.style.display = "none";
-    btnCl.style.display = "block";
+
+function exitInLK(e) {
+    e.preventDefault();
+    btnLogin.classList.remove('not-active');
+    authorizedUserControl.classList.remove('active');
+    btnLogin.classList.add('active');
 }
 
 btnExit.addEventListener("click", exitInLK);
-btn.addEventListener("click", modalOn);
-btnEnter.addEventListener("click", modalOff);
-
+openFormLogin.addEventListener("click", loginFormOn);
+logginBtn.addEventListener("click", loginFormOff);
+modalWindow.addEventListener("click", ((closeModalForm) => loginFormModal.classList.remove('active')));
 
 //  login
-const input = document.querySelector('.form-input_login');
-const password = document.querySelector('.form-input_password');
+const inputLogin = document.querySelector('.form-input_login');
+const inpitPassword = document.querySelector('.form-input_password');
 const log = document.getElementById('aut-input');
-input.addEventListener('input', updateValue);
+inputLogin.addEventListener('input', updateValue);
+
 function updateValue(e) {
     log.textContent = e.target.value;
 }
+
 const inputValue = document.querySelector('.form-input_login').value;
 const passwordValue = document.querySelector('.form-input_password').value;
-
 
 
 //localstorage
@@ -46,23 +53,19 @@ const truefalse = () => {
         console.log(document.querySelector('.form-input_login').value)
     }
 }
-clickCheck.addEventListener( "click",truefalse);
-
-
-
-
+clickCheck.addEventListener("click", truefalse);
 
 
 // tabs
 const tabs = document.querySelectorAll('.nav-item');
 const content = document.querySelectorAll('.content');
-for(let i=0; i < tabs.length; i++) {
-    (function(i) {
+for (let i = 0; i < tabs.length; i++) {
+    (function (i) {
         let tab = tabs[i];
-        tab.addEventListener("click",   function() {
-            for(let j=0; j < content.length; j++) {
+        tab.addEventListener("click", function () {
+            for (let j = 0; j < content.length; j++) {
                 let display = window.getComputedStyle(content[j]).display;
-                if(display === "block") {
+                if (display === "block") {
                     content[j].style.display = "none";
                     tabs[j].classList.remove('nav-item_activ');
 
