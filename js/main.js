17949f22a5c5bd525954baa4d04c63de6367e149
larryself <yanmusikhin@gmail.com>
2021-02-07 20:27:05 +0400

    fixed buds final

--- js/main.js
+++ js/main.js
@@ -1,89 +1,98 @@
+"use strict";
+
 // Открытие модального окна авторизации
-const loginFormModal = document.querySelector(".js-form-modal");
-const openFormLoginBtn = document.querySelector(".js-open-form-btn-open");
+var loginFormModal = document.querySelector(".js-form-modal");
+var openFormLoginBtn = document.querySelector(".js-open-form-btn-open");
 
 function loginFormOn(evt) {
     evt.preventDefault();
     loginFormModal.classList.add("active");
 }
 
-openFormLoginBtn.addEventListener("click", loginFormOn);
+openFormLoginBtn.addEventListener("click", loginFormOn); // Закрытие модального окна при нажатии на оверлей
 
-// Закрытие модального окна при нажатии на оверлей
-const closeLoginFormModal = () => loginFormModal.classList.remove("active");
-const loginFormModalOverlay = document.querySelector(".js-bg-modal");
-loginFormModalOverlay.addEventListener("click", () => closeLoginFormModal());
-
-
-// Работа с localStorage
-const authStateKey = "authState"
-const saveAuthorizedStateToLocalStorage = (username) => {
-    localStorage.setItem(
-        authStateKey,
-        JSON.stringify({
-            authorized: true,
-            username
-        })
-    );
+var closeLoginFormModal = function closeLoginFormModal() {
+    return loginFormModal.classList.remove("active");
 };
-const getAuthorizedState = () => {
-    const auth = localStorage.getItem(authStateKey);
+
+var loginFormModalOverlay = document.querySelector(".js-bg-modal");
+loginFormModalOverlay.addEventListener("click", function () {
+    return closeLoginFormModal();
+}); // Работа с localStorage
+
+var authStateKey = "authState";
+
+var saveAuthorizedStateToLocalStorage = function saveAuthorizedStateToLocalStorage(username) {
+    localStorage.setItem(authStateKey, JSON.stringify({
+        authorized: true,
+        username: username
+    }));
+};
+
+var getAuthorizedState = function getAuthorizedState() {
+    var auth = localStorage.getItem(authStateKey);
+
     if (!auth) {
         return {
             authorized: false
         };
     }
-    const authParsed = JSON.parse(auth);
+
+    var authParsed = JSON.parse(auth);
+
     if (authParsed.authorized) {
         return {
             authorized: true,
             username: authParsed.username
         };
     }
+
     return {
         authorized: false
     };
 };
-const clearAuthState = () => {
+
+var clearAuthState = function clearAuthState() {
     localStorage.clear(authStateKey);
-}
+}; // Авторизация
 
-// Авторизация
-const loginForm = document.querySelector(".js-login-form");
-const authorizedUserBox = document.querySelector(".js-logout-btn-exit");
-const anonymousUserBox = document.querySelector(
-    ".js-open-form-btn-open"
-);
-const userNameBox = document.querySelector(".js-user-name");
-const makeUiAuthorized = (username) => {
 
+var loginForm = document.querySelector(".js-login-form");
+var authorizedUserBox = document.querySelector(".js-logout-btn-exit");
+var anonymousUserBox = document.querySelector(".js-open-form-btn-open");
+var userNameBox = document.querySelector(".js-user-name");
+
+var makeUiAuthorized = function makeUiAuthorized(username) {
     userNameBox.textContent = username;
     authorizedUserBox.classList.add("active");
     anonymousUserBox.classList.remove("active");
-    userNameBox.classList.add("active");   // <====
-}
-const authStatus = getAuthorizedState();
+    userNameBox.classList.add("active"); // <====
+};
+
+var authStatus = getAuthorizedState();
+
 if (authStatus.authorized) {
     makeUiAuthorized(authStatus.username);
 }
 
 function authorize() {
-    const inputLogin = document.querySelector(".form-input-login");
-    const username = inputLogin.value
-    saveAuthorizedStateToLocalStorage(username);
+    var inputLogin = document.querySelector(".form-input-login");
+    var username = inputLogin.value;
+    if (checkbox.checked) {
+        saveAuthorizedStateToLocalStorage(username);
+    }
     makeUiAuthorized(username);
     closeLoginFormModal();
 }
 
-loginForm.addEventListener("submit", (e) => {
+loginForm.addEventListener("submit", function (e) {
     e.preventDefault();
-    authorize();
-    // очищаем форму
-    loginForm.reset();
-});
+    authorize(); // очищаем форму
 
-//Выход из авторизованного состояния
-const btnExit = document.querySelector(".js-logout-btn-exit");
+    loginForm.reset();
+}); //Выход из авторизованного состояния
+
+var btnExit = document.querySelector(".js-logout-btn-exit");
 
 function exitInLK(evt) {
     evt.preventDefault();
@@ -93,17 +102,17 @@ function exitInLK(evt) {
     clearAuthState();
 }
 
-btnExit.addEventListener("click", exitInLK);
+btnExit.addEventListener("click", exitInLK); // tabs
 
-// tabs
-const tabs = document.querySelectorAll(".js-tab");
-const tabContents = document.querySelectorAll(".js-content");
-tabs.forEach((tab) => {
-    tab.addEventListener("click", () => {
+var tabs = document.querySelectorAll(".js-tab");
+var tabContents = document.querySelectorAll(".js-content");
+tabs.forEach(function (tab) {
+    tab.addEventListener("click", function () {
         if (tab.classList.contains("nav-item-active")) {
             return;
         }
-        tabs.forEach((currentTab) => {
+
+        tabs.forEach(function (currentTab) {
             if (currentTab === tab) {
                 currentTab.classList.add("nav-item-active");
             } else {
@@ -111,9 +120,10 @@ tabs.forEach((tab) => {
             }
         });
         tab.classList.add("nav-item-active");
-        const target = tab.dataset.tabTarget;
-        tabContents.forEach((tabContent) => {
-            const isTarget = tabContent.dataset.tabTarget === target;
+        var target = tab.dataset.tabTarget;
+        tabContents.forEach(function (tabContent) {
+            var isTarget = tabContent.dataset.tabTarget === target;
+
             if (isTarget) {
                 tabContent.classList.add("active");
             } else {
@@ -123,15 +133,9 @@ tabs.forEach((tab) => {
     });
 });
 
-// Открытие инпута при нажатии на имя пользователя
-// - при нажатии на имя пользователя показать инпут с таким же значением
-// - при изменении инпута сохранять значение в спан
-// - при изменении инпута сохранять значение в localStorage
-// - при сабмите инпута на enter скрываем инпут и отображаем спан с именем пользователя
-
-const inputChangeUserName = document.querySelector('.js-change-user-name-input')
-const userNameSpan = document.querySelector('.js-user-name');
-const fuckingForma = document.querySelector(".js-formaKissMyAss");
+var inputChangeUserName = document.querySelector('.js-change-user-name-input');
+var userNameSpan = document.querySelector('.js-user-name');
+var formChangeUserName = document.querySelector(".js-form-user-name-change");
 
 function exitNameChange() {
     inputChangeUserName.classList.remove("active");
@@ -142,19 +146,18 @@ function saveNameChange() {
     saveAuthorizedStateToLocalStorage(inputChangeUserName.value);
     userNameSpan.textContent = inputChangeUserName.value;
     inputChangeUserName.addEventListener("blur", exitNameChange);
-    fuckingForma.addEventListener("submit", (evt) => {
+    formChangeUserName.addEventListener("submit", function (evt) {
         evt.preventDefault();
         exitNameChange();
     });
 }
-
+inputChangeUserName.addEventListener("blur", exitNameChange);
+inputChangeUserName.addEventListener("change", saveNameChange);
 function changeUserName() {
     inputChangeUserName.value = userNameSpan.textContent;
     inputChangeUserName.classList.add("active");
     userNameSpan.classList.remove("active");
     inputChangeUserName.focus();
-    inputChangeUserName.addEventListener("blur", exitNameChange);
-    inputChangeUserName.addEventListener("change", saveNameChange);
 }
 
 userNameSpan.addEventListener("click", changeUserName);
