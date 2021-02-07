8be46edeaf11c80b4abc5ed8f36dccc98952a39a
larryself <yanmusikhin@gmail.com>
2021-02-07 18:35:57 +0400

    add js tabs. changename

--- js/main.js
+++ js/main.js
@@ -1,89 +1,161 @@
-// modal window
+// Открытие модального окна авторизации
 const loginFormModal = document.querySelector(".js-form-modal");
-const openFormLogin = document.querySelector(".js-open-form");
-const logginBtn = document.querySelector(".js-autorized-user");
-const authorizedUserControl = document.querySelector(".js-autorized-user-control");
-const btnLogin = document.querySelector(".js-notautorized-user-control");
-const btnExit = document.querySelector(".js-logout");
-const modalWindow = document.querySelector('.js-bg-modal');
+const openFormLoginBtn = document.querySelector(".js-open-form-btn-open");
 
 function loginFormOn(evt) {
     evt.preventDefault();
-    loginFormModal.classList.add('active');
+    loginFormModal.classList.add("active");
 }
 
-function loginFormOff(evt) {
-    evt.preventDefault();
-    loginFormModal.classList.remove('active');
-    authorizedUserControl.classList.add('active');
-    btnLogin.classList.add('not-active');
+openFormLoginBtn.addEventListener("click", loginFormOn);
+
+// Закрытие модального окна при нажатии на оверлей
+const closeLoginFormModal = () => loginFormModal.classList.remove("active");
+const loginFormModalOverlay = document.querySelector(".js-bg-modal");
+loginFormModalOverlay.addEventListener("click", () => closeLoginFormModal());
+
+
+// Работа с localStorage
+const authStateKey = "authState"
+const saveAuthorizedStateToLocalStorage = (username) => {
+    localStorage.setItem(
+        authStateKey,
+        JSON.stringify({
+            authorized: true,
+            username
+        })
+    );
+};
+const getAuthorizedState = () => {
+    const auth = localStorage.getItem(authStateKey);
+    if (!auth) {
+        return {
+            authorized: false
+        };
+    }
+    const authParsed = JSON.parse(auth);
+    if (authParsed.authorized) {
+        return {
+            authorized: true,
+            username: authParsed.username
+        };
+    }
+    return {
+        authorized: false
+    };
+};
+const clearAuthState = () => {
+    localStorage.clear(authStateKey);
 }
+
+// Авторизация
+const loginForm = document.querySelector(".js-login-form");
+const authorizedUserBox = document.querySelector(".js-logout-btn-exit");
+const anonymousUserBox = document.querySelector(
+    ".js-open-form-btn-open"
+);
+const userNameBox = document.querySelector(".js-user-name");
+const makeUiAuthorized = (username) => {
+
+    userNameBox.textContent = username;
+    authorizedUserBox.classList.add("active");
+    anonymousUserBox.classList.remove("active");
+    userNameBox.classList.add("active");   // <====
+}
+const authStatus = getAuthorizedState();
+if (authStatus.authorized) {
+    makeUiAuthorized(authStatus.username);
+}
+
+function authorize() {
+    const inputLogin = document.querySelector(".form-input-login");
+    const username = inputLogin.value
+    saveAuthorizedStateToLocalStorage(username);
+    makeUiAuthorized(username);
+    closeLoginFormModal();
+}
+
+loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    authorize();
+    // очищаем форму
+    loginForm.reset();
+});
+
+//Выход из авторизованного состояния
+const btnExit = document.querySelector(".js-logout-btn-exit");
 
 function exitInLK(evt) {
     evt.preventDefault();
-    btnLogin.classList.remove('not-active');
-    authorizedUserControl.classList.remove('active');
-    btnLogin.classList.add('active');
+    authorizedUserBox.classList.remove("active");
+    anonymousUserBox.classList.add("active");
+    userNameBox.classList.remove("active");
+    clearAuthState();
 }
 
 btnExit.addEventListener("click", exitInLK);
-openFormLogin.addEventListener("click", loginFormOn);
-logginBtn.addEventListener("click", loginFormOff);
-modalWindow.addEventListener("click", ((closeModalForm) => loginFormModal.classList.remove('active')));
-
-//  login
-const inputLogin = document.querySelector('.form-input-login');
-const inputPassword = document.querySelector('.form-input-password');
-const userNameBox = document.querySelector('.js-user-name');
-logginBtn.addEventListener("click", revers);
-function revers() {
-    userNameBox.textContent = inputLogin.value;
-};
-
-//localstorage
-const login = document.getElementsByClassName("form-input-login");
-const clickCheck = document.getElementsByClassName("custom-checkbox")[0];
-const truefalse = () => {
-    if (document.getElementsByClassName("custom-checkbox")[0].checked) {
-        localStorage.setItem("user", inputLoginValue);
-    }
-}
-clickCheck.addEventListener("click", truefalse);
-
 
 // tabs
-// const tabs = document.querySelectorAll('.nav-item');
-// const content = document.querySelectorAll('.content');
-// for (let i = 0; i < tabs.length; i++) {
-//     (function (i) {
-//         let tab = tabs[i];
-//         tab.addEventListener("click", function () {
-//             for (let j = 0; j < content.length; j++) {
-//                 let display = window.getComputedStyle(content[j]).display;
-//                 if (display === "block") {
-//                     content[j].style.display = "none";
-//                     tabs[j].classList.remove('nav-item-activ');
-//
-//                 }
-//             }
-//             content[i].style.display = "block";
-//             tabs[i].classList.add('nav-item-activ')
-//         })
-//     })(i);
-// }
-const tabs = document.querySelectorAll(".nav-item");
-const tabContents = document.querySelectorAll(".content");
-
+const tabs = document.querySelectorAll(".js-tab");
+const tabContents = document.querySelectorAll(".js-content");
 tabs.forEach((tab) => {
     tab.addEventListener("click", () => {
-        const target = document.querySelector(tab.dataset.tabTarget);
+        if (tab.classList.contains("nav-item-active")) {
+            return;
+        }
+        tabs.forEach((currentTab) => {
+            if (currentTab === tab) {
+                currentTab.classList.add("nav-item-active");
+            } else {
+                currentTab.classList.remove("nav-item-active");
+            }
+        });
+        tab.classList.add("nav-item-active");
+        const target = tab.dataset.tabTarget;
         tabContents.forEach((tabContent) => {
-            tabContent.classList.remove("active");
+            const isTarget = tabContent.dataset.tabTarget === target;
+            if (isTarget) {
+                tabContent.classList.add("active");
+            } else {
+                tabContent.classList.remove("active");
+            }
         });
-        tabs.forEach((tab) => {
-            tab.classList.remove("nav-item-activ");
-        });
-        tab.classList.add("nav-item-activ");
-        target.classList.add("not-active");
     });
 });
+
+// Открытие инпута при нажатии на имя пользователя
+// - при нажатии на имя пользователя показать инпут с таким же значением
+// - при изменении инпута сохранять значение в спан
+// - при изменении инпута сохранять значение в localStorage
+// - при сабмите инпута на enter скрываем инпут и отображаем спан с именем пользователя
+
+const inputChangeUserName = document.querySelector('.js-change-user-name-input')
+const userNameSpan = document.querySelector('.js-user-name');
+const fuckingForma = document.querySelector(".js-formaKissMyAss");
+
+function exitNameChange() {
+    inputChangeUserName.classList.remove("active");
+    userNameSpan.classList.add("active");
+}
+
+function saveNameChange() {
+    saveAuthorizedStateToLocalStorage(inputChangeUserName.value);
+    userNameSpan.textContent = inputChangeUserName.value;
+    inputChangeUserName.addEventListener("blur", exitNameChange);
+    fuckingForma.addEventListener("submit", (evt) => {
+        evt.preventDefault();
+        exitNameChange();
+    });
+
+}
+
+function changeUserName() {
+    inputChangeUserName.value = userNameSpan.textContent;
+    inputChangeUserName.classList.add("active");
+    userNameSpan.classList.remove("active");
+    inputChangeUserName.focus();
+    inputChangeUserName.addEventListener("blur", exitNameChange);
+    inputChangeUserName.addEventListener("change", saveNameChange);
+}
+
+userNameSpan.addEventListener("click", changeUserName);
